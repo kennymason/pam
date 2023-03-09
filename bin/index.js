8eb31f00a13426311051850277150ed4b26376f1
Kenneth Mason <kennymmase@gmail.com>
2023-03-08 23:26:50 -0500

    Working GPT3.5 interface

--- bin/index.js
+++ bin/index.js
@@ -6,43 +6,102 @@ const utils = require('./utils.js')
 const chalk = require('chalk');  
 const boxen = require('boxen');
 
-// all non-flag args get stored in the list 'yargs.argv._'
-// set demandOption to true to make the option compulsory. yargs will then throw a missing argument error if flag is not provided
-const usage = chalk.hex('#83aaff')("\nUsage: tran <lang_name> sentence to be translated");
-const options = yargs  
-      .usage(usage)  
-      .option("l", {
-        alias:"languages",
-        describe: "List all supported languages.",
-        type: "boolean",
-        demandOption : false
-      })                                                                                                    
-      .help(true)  
-      .argv;
+// main();
 
-var sentence = utils.parseSentence(yargs.argv._);
+(async function main () {
+  // all non-flag args get stored in the list 'yargs.argv._'
+  // set demandOption to true to make the option compulsory. yargs will then throw a missing argument error if flag is not provided
+  const usage = chalk.hex('#83aaff')("\nUsage: pam <option> <prompt>");
+  const options = yargs  
+        .usage(usage)  
+        .option("c", {
+          alias:"chat",
+          describe: "Enter GPT chat",
+          type: "boolean",
+          demandOption : false
+        })   
+        .option("o", {
+          alias:"options",
+          describe: "List available options",
+          type: "boolean",
+          demandOption : false
+        })                                                                                                    
+        .help(true)
+        .argv;
 
-if(yargs.argv._[0] == null){  
-  utils.showHelp();  
-  return;  
-}
+  // if options flag set, list options
+  if(yargs.argv.o == true || yargs.argv.options == true){  
+    utils.showAll();
+    return;  
+  }
 
-if(yargs.argv.l == true || yargs.argv.languages == true){  
-  utils.showAll();  
-  return;  
-}
+  // get primary arg
+  if(yargs.argv._[0]) {
+    var firstArg = yargs.argv._[0].toLowerCase();
+  }
 
-if(yargs.argv._[0])  
-var language = yargs.argv._[0].toLowerCase(); // stores the language.
-//parsing the language specified to the ISO-639-1 code.                                                                                              
-language = utils.parseLanguage(language);
+  // TODO update this
+  if (firstArg == 'chat' || yargs.argv.c == true || yargs.argv.chat == true) {
+    if (firstArg == 'chat'){
+      var sentence = utils.parseSentence(yargs.argv._, 1);
+    }
+    else {
+      var sentence = utils.parseSentence(yargs.argv._, 0);
+    }
 
-if(sentence == ""){                                                                                          
-    console.error("\nThe entered sentence is like John Cena, I can't see it!\n")  
-    console.log("Enter tran --help to get started.\n")  
+    await startChat(sentence);
     return;
+  }
+  else if(yargs.argv._[0] == null){  
+    // if no args give, show help
+    utils.showHelp();  
+    return;  
+  }
+  else {
+    console.log("Sorry! not supported yet.");
+    return;
+  }
+})();
+
+async function startChat (startingPrompt) {
+  console.log("PAM powered by gpt-3.5-turbo\nType 'exit' to quit");
+
+  let prompt = startingPrompt;
+  while (prompt = "") {
+    prompt = await utils.askQuestion("user>")
+      .then(res => {
+        return res
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    if (prompt == 'exit' || prompt == 'quit') return;
+  }
+
+  while (true) {
+    await utils.askGPT(prompt)
+    .then(res => {
+      console.log("\n" + boxen(chalk.green(res + "\n"), {padding: 1, borderColor: 'green', dimBorder: true}) + "\n");
+    })
+    .catch(err => {                            
+      // console.error(err);  
+      console.log("\n");
+    });
+
+    prompt = await utils.askQuestion("user>")
+      .then(res => {
+        return res
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    if (prompt == "exit" || prompt == "quit") return;
+  }
 }
-console.log("\n" + boxen(chalk.green("\nTranslation here\n"), {padding: 1, borderColor: 'green', dimBorder: true}) + "\n");
+
+
 // translate(sentence, {to: language})
 //   .then(res => {
 //     console.log("\n" + boxen(chalk.green("\n" + res.text + "\n"), {padding: 1, borderColor: 'green', dimBorder: true}) + "\n");
